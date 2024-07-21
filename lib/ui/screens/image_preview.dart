07b8b021ce1f0fefedee269414532b7e98aaaf51
Shantanu Borkar <shantanu.b@arishti.in>
2024-07-22 04:27:45 +0530

    add: share image support

--- lib/ui/screens/image_preview.dart
+++ lib/ui/screens/image_preview.dart
@@ -6,6 +6,8 @@ import 'package:file_encrypt/application/bloc/encryption/encryption_state.dart';
 import 'package:file_encrypt/core/services/encryption_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_logs/flutter_logs.dart';
+import 'package:share_plus/share_plus.dart';
 
 class ImagePreviewScreen extends StatefulWidget {
   const ImagePreviewScreen({super.key});
@@ -26,14 +28,57 @@ class _ImagePreviewScreenState extends State<ImagePreviewScreen> {
               title: const Text("Image Preview"),
               actions: [
                 IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                      context.read<EncryptionBloc>().add(
-                          EncryptionBlocEvent.decryptImage(
-                              image: state.currentPreviewImageModel!,
-                              context: context));
-                    },
-                    icon: const Icon(Icons.restore))
+                  onPressed: () {
+                    Navigator.pop(context);
+                    context.read<EncryptionBloc>().add(
+                        EncryptionBlocEvent.decryptImage(
+                            image: state.currentPreviewImageModel!,
+                            context: context));
+                  },
+                  icon: const Icon(Icons.restore),
+                ),
+                IconButton(
+                  onPressed: () {
+                    // context.read<EncryptionBloc>().add(
+                    //     EncryptionBlocEvent.decryptImage(
+                    //         image: state.currentPreviewImageModel!,
+                    //         context: context));
+                    FlutterLogs.logInfo(
+                        "Image Share Selected",
+                        state.currentPreviewImageModel!.imageName,
+                        state.currentPreviewImageModel!.dateCreated
+                            .toIso8601String());
+                    XFile currentImage =
+                        XFile.fromData(state.currentPreviewImage!);
+                    Share.shareXFiles([currentImage])
+                        .then((ShareResult shareResult) {
+                      if (shareResult.status == ShareResultStatus.success) {
+                        FlutterLogs.logInfo(
+                          "Image Shared Successfully ",
+                          state.currentPreviewImageModel!.imageName,
+                          state.currentPreviewImageModel!.dateCreated
+                              .toIso8601String(),
+                        );
+                      } else if (shareResult.status ==
+                          ShareResultStatus.dismissed) {
+                        FlutterLogs.logInfo(
+                          "Image Share Dismissed ",
+                          state.currentPreviewImageModel!.imageName,
+                          state.currentPreviewImageModel!.dateCreated
+                              .toIso8601String(),
+                        );
+                      } else {
+                        FlutterLogs.logInfo(
+                          "Image Share Unavailable ",
+                          state.currentPreviewImageModel!.imageName,
+                          state.currentPreviewImageModel!.dateCreated
+                              .toIso8601String(),
+                        );
+                      }
+                    });
+                  },
+                  icon: const Icon(Icons.share),
+                ),
               ],
             ),
             body: state.isLoading
