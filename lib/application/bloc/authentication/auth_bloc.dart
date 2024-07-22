e80df3493dd4d505c913dcaaf52deff91b5de65d
Shantanu Borkar <shantanu.b@arishti.in>
2024-07-22 16:47:09 +0530

    add: Logs added

--- lib/application/bloc/authentication/auth_bloc.dart
+++ lib/application/bloc/authentication/auth_bloc.dart
@@ -7,6 +7,7 @@ import 'package:file_encrypt/application/bloc/authentication/auth_event.dart';
 import 'package:file_encrypt/application/bloc/authentication/auth_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_logs/flutter_logs.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:local_auth/local_auth.dart';
 
@@ -27,54 +28,74 @@ class AuthBloc extends Bloc<AuthBlocEvent, AuthBlocState> {
 
           bool isPin = await secureStorage.containsKey(key: "user_pin");
           emit(state.copyWith(isLoading: false, isExistingUser: isPin));
+          FlutterLogs.logInfo("AuthBloc Log", "Action: Init", "");
         },
         selectAuthMethod: (AuthMethod authMethod) {
           if (authMethod == AuthMethod.face ||
               authMethod == AuthMethod.fingerprint) {}
           emit(state.copyWith(authMethod: authMethod));
+          FlutterLogs.logInfo("AuthBloc Log", "Action: Select Auth Method", "");
         },
         enterPin: (String enteredPin, BuildContext context) async {
           emit(state.copyWith(isLoading: true));
-          if (state.isExistingUser) {
-            Uint8List pinToBytes = utf8.encode(enteredPin);
-            String enteredPinHash = sha256.convert(pinToBytes).toString();
-            await secureStorage.read(key: "user_pin").then((String? pinHash) {
-              if (pinHash! == enteredPinHash) {
-                emit(state.copyWith(
-                    isAuthenticated: true, isLoading: false, enteredPin: null));
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text("Incorrect Pin")));
-              }
-            });
+          try {
+            if (state.isExistingUser) {
+              Uint8List pinToBytes = utf8.encode(enteredPin);
+              String enteredPinHash = sha256.convert(pinToBytes).toString();
+              await secureStorage.read(key: "user_pin").then((String? pinHash) {
+                if (pinHash! == enteredPinHash) {
+                  emit(state.copyWith(
+                      isAuthenticated: true,
+                      isLoading: false,
+                      enteredPin: null));
+                  FlutterLogs.logInfo("AuthBloc Log", "Action: Enter Pin",
+                      "Old User Authenticated");
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Incorrect Pin")));
+                  FlutterLogs.logInfo(
+                      "AuthBloc Log", "Action: Enter Pin", "Incorrect Pin");
+                }
+              });
 
-            return;
-          } else {
-            Uint8List pinToBytes = utf8.encode(enteredPin);
-            String enteredPinHash = sha256.convert(pinToBytes).toString();
-            await secureStorage.write(key: "user_pin", value: enteredPinHash);
-            emit(state.copyWith(
-                isAuthenticated: true, enteredPin: null, isLoading: false));
+              return;
+            } else {
+              Uint8List pinToBytes = utf8.encode(enteredPin);
+              String enteredPinHash = sha256.convert(pinToBytes).toString();
+              await secureStorage.write(key: "user_pin", value: enteredPinHash);
+              emit(state.copyWith(
+                  isAuthenticated: true, enteredPin: null, isLoading: false));
+              FlutterLogs.logInfo("AuthBloc Log", "Action: Enter Pin",
+                  "New User Authenticated");
+            }
+          } catch (e) {
+            FlutterLogs.logInfo(
+                "AuthBloc Log", "Enter Pin Error", e.toString());
           }
-          emit(state.copyWith(isLoading: false));
         },
         resetPin: (String newPin, BuildContext context) async {
           emit(state.copyWith(isLoading: true));
+          try {
+            Uint8List pinToBytes = utf8.encode(newPin);
+            String enteredPinHash = sha256.convert(pinToBytes).toString();
 
-          Uint8List pinToBytes = utf8.encode(newPin);
-          String enteredPinHash = sha256.convert(pinToBytes).toString();
-
-          await secureStorage
-              .write(key: "user_pin", value: enteredPinHash)
-              .then((value) {
-            emit(
-              state.copyWith(
-                  isAuthenticated: true, enteredPin: null, isLoading: false),
-            );
-            ScaffoldMessenger.of(context)
-                .showSnackBar(const SnackBar(content: Text("Pin Changed")));
-            Navigator.pop(context);
-          });
+            await secureStorage
+                .write(key: "user_pin", value: enteredPinHash)
+                .then((value) {
+              emit(
+                state.copyWith(
+                    isAuthenticated: true, enteredPin: null, isLoading: false),
+              );
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(const SnackBar(content: Text("Pin Changed")));
+              FlutterLogs.logInfo(
+                  "AuthBloc Log", "Action: Reset Pin", "Pin Changed");
+              Navigator.pop(context);
+            });
+          } catch (e) {
+            FlutterLogs.logInfo(
+                "AuthBloc Log", "Reset Pin Error", e.toString());
+          }
         },
       );
     });
