8233f6c6e1fb9f515f6f1f9a4e5b91e95837122a
Shantanu Borkar <shantanu.b@arishti.in>
2024-07-22 16:36:51 +0530

    add: AuthBloc enabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:file_encrypt/application/bloc/authentication/auth_bloc.dart';
+import 'package:file_encrypt/application/bloc/authentication/auth_state.dart';
+import 'package:file_encrypt/ui/screens/auth/pin_input_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_logs/flutter_logs.dart';
 
 import 'package:file_encrypt/application/bloc/encryption/encryption_bloc.dart';
 import 'package:file_encrypt/core/database/objectbox.dart';
-import 'package:file_encrypt/core/services/logger.dart';
-import 'package:file_encrypt/ui/screens/home/home_screen.dart';
+import 'package:file_encrypt/core/utils/logger.dart';
+import 'package:file_encrypt/ui/screens/home_screen.dart';
 
 late final ObjectBoxAdapter objectbox;
 
@@ -13,20 +16,21 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await FlutterLogs.initLogs(
-      logLevelsEnabled: [
-        LogLevel.INFO,
-        LogLevel.WARNING,
-        LogLevel.ERROR,
-        LogLevel.SEVERE
-      ],
-      timeStampFormat: TimeStampFormat.TIME_FORMAT_READABLE,
-      directoryStructure: DirectoryStructure.FOR_DATE,
-      logTypesEnabled: ["device", "encryption", "image access", "errors"],
-      logFileExtension: LogFileExtension.LOG,
-      logsWriteDirectoryName: "AppLogs",
-      logsExportDirectoryName: "AppLogs/Exported",
-      debugFileOperations: true,
-      isDebuggable: true);
+    logLevelsEnabled: [
+      LogLevel.INFO,
+      LogLevel.WARNING,
+      LogLevel.ERROR,
+      LogLevel.SEVERE
+    ],
+    timeStampFormat: TimeStampFormat.TIME_FORMAT_READABLE,
+    directoryStructure: DirectoryStructure.FOR_DATE,
+    logTypesEnabled: ["device", "encryption", "image access", "errors"],
+    logFileExtension: LogFileExtension.LOG,
+    logsWriteDirectoryName: "AppLogs",
+    logsExportDirectoryName: "AppLogs/Exported",
+    debugFileOperations: true,
+    isDebuggable: true,
+  );
 
   FlutterLogs.logInfo("App Start", DateTime.now().toIso8601String(), "");
   objectbox = await ObjectBoxAdapter.create();
@@ -42,11 +46,26 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
+          lazy: false,
           create: (context) => EncryptionBloc(objectBoxAdapter: objectbox),
         ),
+        BlocProvider(lazy: false, create: (context) => AuthBloc())
       ],
-      child: const MaterialApp(
-        home: HomeScreen(),
+      child: MaterialApp(
+        theme: ThemeData(useMaterial3: false),
+        home: BlocConsumer<AuthBloc, AuthBlocState>(
+          listener: (context, state) {
+            if (state.isAuthenticated) {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const HomeScreen()),
+              );
+            }
+          },
+          builder: (context, state) {
+            return const PinInputScreen();
+          },
+        ),
       ),
     );
   }
